f492dd66a8a9a97ed293f90c22e081255fae8a04
azizrahimov2509 <rahimov2509@mail.com>
2024-06-09 23:34:19 +0300

    Updated

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,8 +8,7 @@ function Header() {
     data: [data],
     isPending,
     error,
-  } = useGetData("cart", "cart", null);
-  console.log(data);
+  } = useGetData("cart", refresh);
   const navigate = useNavigate();
   const user = JSON.parse(localStorage.getItem("user"));
 
@@ -38,8 +37,8 @@ function Header() {
   };
 
   return (
-    <header className=" bg-base-300">
-      <div className="container  navbar">
+    <header className="bg-base-300">
+      <div className="container navbar">
         <div className="navbar-start">
           <div className="dropdown">
             <div
@@ -124,7 +123,10 @@ function Header() {
                     />
                   </svg>
                   <span className="badge badge-primary badge-sm indicator-item">
-                    {data?.products.length ?? 0}
+                    {data?.products.reduce(
+                      (acc, item) => acc + item.count,
+                      0
+                    ) ?? 0}
                   </span>
                 </div>
               </div>
@@ -134,9 +136,12 @@ function Header() {
               >
                 <div className="card-body">
                   <span className="font-bold text-lg">
-                    {data?.products.length ?? 0} Items
+                    {data?.products.reduce(
+                      (acc, item) => acc + item.count,
+                      0
+                    ) ?? 0}{" "}
+                    Items
                   </span>
-
                   <div className="card-actions">
                     <button className="btn btn-primary btn-block">
                       View cart
@@ -154,8 +159,6 @@ function Header() {
               onChange={handleToggle}
               checked={theme === "dark"}
             />
-
-            {/* Sun icon */}
             <svg
               className="swap-off fill-current w-10 h-10"
               xmlns="http://www.w3.org/2000/svg"
@@ -164,7 +167,6 @@ function Header() {
               <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
             </svg>
 
-            {/* Moon icon */}
             <svg
               className="swap-on fill-current w-10 h-10"
               xmlns="http://www.w3.org/2000/svg"
--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import useGetData from "../../hooks/useGetData";
 import ProductModal from "../../components/ProductModal";
 import { updateDoc, arrayUnion, deleteDoc, doc } from "firebase/firestore";
@@ -8,14 +8,14 @@ export default function Products() {
   const [filter, setFilter] = useState("rating");
   const [refresh, setRefresh] = useState(false);
   const { data, isPending, error } = useGetData("products", refresh, filter);
-  console.log(data);
+  const [cartItems, setCartItems] = useState({});
+
   const handleDelete = async (id) => {
     if (window.confirm("Are you sure you want to delete this product?")) {
       try {
         document.getElementById("my_modal_1").showModal();
         await deleteDoc(doc(db, "products", id));
         setRefresh((prev) => !prev);
-
         document.getElementById("my_modal_1").closest("dialog").close();
       } catch (error) {
         console.error("Error deleting product: ", error);
@@ -23,27 +23,64 @@ export default function Products() {
     }
   };
 
-  const handleAddToCart = async (id) => {
+  const handleAddToCart = (id) => {
+    setCartItems((prev) => ({
+      ...prev,
+      [id]: (prev[id] || 0) + 1,
+    }));
+  };
+
+  const handleUpdateCart = async (id) => {
     const ref = doc(db, "cart", "Sq9hZ7Mo4guHBgvkeuMC");
     document.getElementById("my_modal_1").showModal();
-    await updateDoc(ref, {
-      products: arrayUnion({
-        productId: id,
-        count: 1,
-      }),
-    }),
+    try {
+      await updateDoc(ref, {
+        products: arrayUnion({
+          productId: id,
+          count: cartItems[id],
+        }),
+      });
       document.getElementById("my_modal_1").closest("dialog").close();
+      setRefresh((prev) => !prev);
+      window.location.reload();
+    } catch (error) {
+      console.error("Error adding product to cart: ", error);
+      document.getElementById("my_modal_1").closest("dialog").close();
+    }
   };
+
+  const handleIncrement = (id) => {
+    setCartItems((prev) => ({
+      ...prev,
+      [id]: (prev[id] || 0) + 1,
+    }));
+  };
+
+  const handleDecrement = (id) => {
+    setCartItems((prev) => {
+      const newCount = (prev[id] || 0) - 1;
+      if (newCount < 1) {
+        const { [id]: _, ...rest } = prev;
+        return rest;
+      } else {
+        return {
+          ...prev,
+          [id]: newCount,
+        };
+      }
+    });
+  };
+
   return (
     <section>
-      <div className="container ">
+      <div className="container">
         <div className="flex items-center justify-between pb-3 border-b-2 mb-10">
           <h2 className="text-2xl">Products</h2>
           <div className="flex items-center gap-4">
             <select
               value={filter}
               onChange={(e) => setFilter(e.target.value)}
-              className="select  select-sm select-bordered w-full max-w-xs"
+              className="select select-sm select-bordered w-full max-w-xs"
             >
               <option value="rating">Rating⭐</option>
               <option value="price">Price $</option>
@@ -51,7 +88,7 @@ export default function Products() {
               <option value={"!name"}>Z-A</option>
             </select>
             <button
-              className="btn btn-success  btn-sm"
+              className="btn btn-success btn-sm"
               onClick={() => document.getElementById("my_modal_3").showModal()}
             >
               Add
@@ -60,7 +97,7 @@ export default function Products() {
         </div>
 
         {isPending && (
-          <div className=" mt-14 flex items-center justify-center">
+          <div className="mt-14 flex items-center justify-center">
             <span
               style={{ zoom: 2 }}
               className="loading loading-spinner loading-lg"
@@ -80,16 +117,46 @@ export default function Products() {
                 </figure>
                 <div className="card-body">
                   <h2 className="card-title text-lg">{name}</h2>
-                  <p> Price: {price}$</p>
+                  <p>Price: {price}$</p>
                   <p>Rating: {rating}⭐</p>
-                  <p>Descriprion: {desc}</p>
+                  <p>Description: {desc}</p>
                   <div className="card-actions justify-end">
-                    <button
-                      className="btn btn-sm btn-primary"
-                      onClick={() => handleAddToCart(id)}
-                    >
-                      Buy Now
-                    </button>
+                    {cartItems[id] ? (
+                      <div className="flex items-center">
+                        <button
+                          className="btn btn-sm btn-secondary"
+                          onClick={() => handleDecrement(id)}
+                          disabled={cartItems[id] <= 1}
+                        >
+                          -
+                        </button>
+                        <input
+                          type="text"
+                          value={cartItems[id]}
+                          readOnly
+                          className="mx-2 w-12 text-center"
+                        />
+                        <button
+                          className="btn btn-sm btn-secondary"
+                          onClick={() => handleIncrement(id)}
+                        >
+                          +
+                        </button>
+                        <button
+                          className="btn btn-sm btn-primary ml-2"
+                          onClick={() => handleUpdateCart(id)}
+                        >
+                          Add to Cart
+                        </button>
+                      </div>
+                    ) : (
+                      <button
+                        className="btn btn-sm btn-primary"
+                        onClick={() => handleAddToCart(id)}
+                      >
+                        Buy Now
+                      </button>
+                    )}
                     <button
                       className="btn btn-sm btn-error"
                       onClick={() => handleDelete(id)}
@@ -105,8 +172,6 @@ export default function Products() {
       </div>
 
       <ProductModal setRefresh={setRefresh} />
-      {/* Delete Modal */}
-
       <dialog id="my_modal_1" className="modal">
         <div className="modal-box w-fit">
           <span className="loading loading-spinner loading-lg"></span>
